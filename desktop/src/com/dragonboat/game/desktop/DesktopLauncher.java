adf497eb75490ae9dde147fc52e1757375dfb862
Joe  Lonsdale <jl3067@york.ac.uk>
2020-10-27 16:51:02 +0000

    Files aside from class files we need to share (I think)

---
+++ desktop/src/com/dragonboat/game/desktop/DesktopLauncher.java
@@ -0,0 +1,12 @@
+package com.dragonboat.game.desktop;
+
+import com.badlogic.gdx.backends.lwjgl.LwjglApplication;
+import com.badlogic.gdx.backends.lwjgl.LwjglApplicationConfiguration;
+import com.dragonboat.game.DragonBoatGame;
+
+public class DesktopLauncher {
+	public static void main (String[] arg) {
+		LwjglApplicationConfiguration config = new LwjglApplicationConfiguration();
+		new LwjglApplication(new DragonBoatGame(), config);
+	}
+}
