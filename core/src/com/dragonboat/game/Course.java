c3415131fd260982704f682dae8a5c273f1c82e1
James Wilkinson <jrm567@york.ac.uk>
2020-10-28 21:51:08 +0000

    Initial commit for Course class

---
+++ core/src/com/dragonboat/game/Course.java
@@ -0,0 +1,11 @@
+package com.dragonboat.game;
+
+public class Course {
+	public Lane[] lanes;
+	public int startY, finishY, leftBoundary, rightBoundary;
+	public Texture texture;
+	
+	public Course() {
+		
+	}
+}
