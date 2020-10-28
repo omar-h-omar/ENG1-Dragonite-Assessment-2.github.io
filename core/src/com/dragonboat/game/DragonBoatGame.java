5813f3d5cba354860812b00c790ccaacc7f111a4
Joe  Lonsdale <jl3067@york.ac.uk>
2020-10-28 18:48:00 +0000

    Updated some initial environment variables.

--- core/src/com/dragonboat/game/DragonBoatGame.java
+++ core/src/com/dragonboat/game/DragonBoatGame.java
@@ -2,32 +2,41 @@ package com.dragonboat.game;
 
 import com.badlogic.gdx.ApplicationAdapter;
 import com.badlogic.gdx.Gdx;
+import com.badlogic.gdx.graphics.Camera;
 import com.badlogic.gdx.graphics.GL20;
+import com.badlogic.gdx.graphics.OrthographicCamera;
 import com.badlogic.gdx.graphics.Texture;
 import com.badlogic.gdx.graphics.g2d.SpriteBatch;
 
 public class DragonBoatGame extends ApplicationAdapter {
 	SpriteBatch batch;
-	Texture img;
-	
+	private OrthographicCamera camera;
 	@Override
 	public void create () {
 		batch = new SpriteBatch();
-		img = new Texture("badlogic.jpg");
+		float w = Gdx.graphics.getWidth();
+		float h = Gdx.graphics.getHeight();
+
+		camera = new OrthographicCamera();
+		camera.setToOrtho(false, w / 2, h / 2);
 	}
 
 	@Override
 	public void render () {
-		Gdx.gl.glClearColor(1, 0, 0, 1);
+		Gdx.gl.glClearColor(0, 0, 0, 1);
 		Gdx.gl.glClear(GL20.GL_COLOR_BUFFER_BIT);
 		batch.begin();
-		batch.draw(img, 0, 0);
+
 		batch.end();
 	}
-	
+
+	@Override
+	public void resize(int width, int height) {
+		camera.setToOrtho(false, width / 2, height / 2);
+	}
+
 	@Override
 	public void dispose () {
 		batch.dispose();
-		img.dispose();
 	}
 }
--- desktop/src/com/dragonboat/game/desktop/DesktopLauncher.java
+++ desktop/src/com/dragonboat/game/desktop/DesktopLauncher.java
@@ -7,6 +7,10 @@ import com.dragonboat.game.DragonBoatGame;
 public class DesktopLauncher {
 	public static void main (String[] arg) {
 		LwjglApplicationConfiguration config = new LwjglApplicationConfiguration();
+		config.width = 1080;
+		config.height = 720;
+		config.backgroundFPS = 60;
+		config.foregroundFPS = 60;
 		new LwjglApplication(new DragonBoatGame(), config);
 	}
 }
