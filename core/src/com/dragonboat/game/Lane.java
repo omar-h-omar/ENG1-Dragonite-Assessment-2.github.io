6c688857272a161d07b601fadb78bd99f54e4969
Joe  Lonsdale <jl3067@york.ac.uk>
2020-10-29 17:06:01 +0000

    Defined SpawnObstacle() and changed this.obstacles to a java ArrayList.

--- core/src/com/dragonboat/game/Lane.java
+++ core/src/com/dragonboat/game/Lane.java
@@ -1,30 +1,43 @@
 package com.dragonboat.game;
 
 import com.sun.corba.se.impl.io.TypeMismatchException;
-import org.omg.IOP.CodecPackage.TypeMismatch;
+
+import java.util.ArrayList;
+
 
 public class Lane {
     private final int LEFTBOUNDARY, RIGHTBOUNDARY;
-    private Obstacle[] obstacles;
+    private ArrayList<Obstacle> obstacles;
     private int obstacleLimit;
     public Lane(int leftBoundary, int rightBoundary) {
        this.LEFTBOUNDARY = leftBoundary;
        this.RIGHTBOUNDARY = rightBoundary;
        this.obstacleLimit = 10;
 
-       obstacles = new Obstacle[obstacleLimit];
+       obstacles = new ArrayList<>();
     }
 
     public void SpawnObstacle(int x, int y, String obstacleType) {
-        if(obstacleType.equals("Goose")) {
+        /*
+        Method for spawning an Obstacle of specified type in this lane.
+        First we check if we are at the obstacle limit, if we are not then
+        we check to see if the specified type of Obstacle is either Goose or Log,
+        if it is we create a new Obstacle instance of either Goose or Log and add
+        it to this Lane's Obstacle list.
+         */
+        if(this.obstacles.size() <= this.obstacleLimit) {
+            if (obstacleType.equals("Goose")) {
+                Goose goose = new Goose(x, y);
+                this.obstacles.add(goose);
 
-        }
-        if(obstacleType.equals("Log")) {
+            } else if (obstacleType.equals("Log")) {
+                Log log = new Log(x, y);
+                this.obstacles.add(log);
 
-        }
-        else {
-            throw new TypeMismatchException("Obstacle type not valid.");
-        }
+            } else {
+                throw new TypeMismatchException("Obstacle type not valid.");
+            }
+        } else System.console().printf("Obstacle limit reached.");
     }
 
     // getters and setters
