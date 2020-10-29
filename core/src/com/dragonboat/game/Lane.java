1e5fe185254b3523cb2a88c6550d6b70e5e12dec
Joe  Lonsdale <jl3067@york.ac.uk>
2020-10-29 16:37:13 +0000

    Implemented basic Lane class.

--- core/src/com/dragonboat/game/Lane.java
+++ core/src/com/dragonboat/game/Lane.java
@@ -1,14 +1,38 @@
 package com.dragonboat.game;
 
+import com.sun.corba.se.impl.io.TypeMismatchException;
+import org.omg.IOP.CodecPackage.TypeMismatch;
+
 public class Lane {
-    public Lane() {
+    private final int LEFTBOUNDARY, RIGHTBOUNDARY;
+    private Obstacle[] obstacles;
+    private int obstacleLimit;
+    public Lane(int leftBoundary, int rightBoundary) {
+       this.LEFTBOUNDARY = leftBoundary;
+       this.RIGHTBOUNDARY = rightBoundary;
+       this.obstacleLimit = 10;
 
+       obstacles = new Obstacle[obstacleLimit];
     }
 
-    public int getLeftBoundary() {
-        return this.leftBoundary;
+    public void SpawnObstacle(int x, int y, String obstacleType) {
+        if(obstacleType.equals("Goose")) {
+
+        }
+        if(obstacleType.equals("Log")) {
+
+        }
+        else {
+            throw new TypeMismatchException("Obstacle type not valid.");
+        }
     }
-    public int getRightBoundary() {
-        return this.rightBoundary;
+
+    // getters and setters
+
+    public int GetLeftBoundary() {
+        return this.LEFTBOUNDARY;
+    }
+    public int GetRightBoundary() {
+        return this.RIGHTBOUNDARY;
     }
 }
