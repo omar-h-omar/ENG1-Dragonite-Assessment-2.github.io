0aa8b6104d2bb2c4e7c66036c3204ce2ad30e369
will <rwd1@hotmail.co.uk>
2020-11-02 23:34:18 +0000

    Leaderboard updated

--- core/src/com/dragonboat/game/Leaderboard.java
+++ core/src/com/dragonboat/game/Leaderboard.java
@@ -1,10 +1,54 @@
 package com.dragonboat.game;
 
-import java.util.HashMap;
+import java.util.Arrays;
+import java.util.Comparator;
 
 public class Leaderboard {
     //attributes
     private Boat[] boats;
+    private Boat[] sortedBoats;
+    private int[] raceTimes;
+    static private Comparator<Boat> ascRaceTime;
 
-    // public
+    public Leaderboard(Boat[] boats){
+        this.boats = boats;
+        this.sortedBoats = boats;
+    }
+
+    private void updateTimes(){
+        Arrays.sort(this.sortedBoats, ascRaceTime);
+    }
+
+    public Boat[] GetFinalists(){
+        Boat[] finalists = new Boat[4];
+        for(int i = 0; i < 4; i++){
+            finalists[i] = sortedBoats[i];
+        }
+
+        return finalists;
+    }
+
+    //custom number of places
+    public Boat[] GetFinalists(int places){
+        Boat[] finalists = new Boat[places];
+        for(int i = 0; i < places; i++){
+            finalists[i] = sortedBoats[i];
+        }
+
+        return finalists;
+    }
+
+    public Boat[] GetPodium(){
+        return GetFinalists(3);
+    }
+
+    static{
+        //allows boat times to be compared
+        ascRaceTime = new Comparator<Boat>(){
+            @Override
+            public int compare(Boat boat1, Boat boat2){
+                return Long.compare(boat1.GetFastestTime(), boat2.GetFastestTime());
+            }
+        };
+    }
 }
