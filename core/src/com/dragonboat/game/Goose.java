423847e4f330abccee8507acdbb69c5217d7de92
James Wilkinson <jrm567@york.ac.uk>
2020-11-03 10:31:44 +0000

    Changes to paramater types to Obstacle and added texture parameter to Goose

--- core/src/com/dragonboat/game/Goose.java
+++ core/src/com/dragonboat/game/Goose.java
@@ -2,7 +2,7 @@ package com.dragonboat.game;
 
 public class Goose extends Obstacle {
 	
-	public Goose(int xPosition, int yPosition) {
+	public Goose(int xPosition, int yPosition, Texture texture) {
 		// Joe: geese will have a set damage value, could increase with difficulty.
 		//      they'll also have a set width and height. just put some placeholders in for now.
 		super(5, xPosition, yPosition, 10, 10);
