fe6264f3d764a284e93e558313ed4e1ebda4d4f3
will <rwd1@hotmail.co.uk>
2020-11-05 13:24:27 +0000

    Reduced ProgressBar methods, implemented by Boat

--- core/src/com/dragonboat/game/ProgressBar.java
+++ core/src/com/dragonboat/game/ProgressBar.java
@@ -8,56 +8,22 @@ public class ProgressBar {
 
 	private Player playerBoat;
 	private Opponent[] opponnentBoats;
-	private int courseLength;
-	private float playerProgress;
-	private float[] opponentProgress;
 	private float timeSeconds;
 	private float playerTime;
-	private int finishLine;
 
 	
 	public ProgressBar(Player player, Opponent[] opponents, Course course) {
 		this.playerBoat = player;
 		this.opponnentBoats = opponents;
-
-		this.courseLength = course.finishY - course.startY;
-		
-		//fill progress with empty
-		this.playerProgress = 0f;
-		for(int i = 0; i < this.opponnentBoats.length; i++){
-			this.opponentProgress[i] = 0f;
-		}
-	}
-	
-	//update progress for all boats
-	public void UpdateProgress() {
-		//update player
-		if(this.playerBoat.GetY() < this.finishLine){
-			this.playerProgress = this.playerBoat.GetY() / this.courseLength;
-		}else{
-			this.playerProgress = 1f;
-		}
-		
-		//update opponents
-		for(int i = 0; i < this.opponnentBoats.length; i++){
-			if(this.opponnentBoats[i].GetY() < this.finishLine){
-				this.opponentProgress[i] = this.opponnentBoats[i].GetY() / this.courseLength;
-			}else{
-				this.opponentProgress[i] = 1f;
-			}
-		}
 	}
 
 	//return boat positions
 	//player position, followed by all others
 	public float[] GetProgress(){
-		//make sure positions are up to data
-		UpdateProgress();
-
 		float[] out = new float[opponnentBoats.length + 1];
-		out[0] = playerProgress;
+		out[0] = playerBoat.progress;
 		for(int i = 0; i < opponnentBoats.length; i++){
-			out[i + 1] = opponentProgress[i];
+			out[i + 1] = opponnentBoats[i].progress;
 		}
 
 		return out;
