33d4a63c787be54ff6cb9fc7e0ed9b8247581193
Ben <bdj503@york.ac.uk>
2020-11-12 15:33:08 +0000

    goose moves properly

--- core/src/com/dragonboat/game/Goose.java
+++ core/src/com/dragonboat/game/Goose.java
@@ -10,13 +10,15 @@ import java.util.Random;
 public class Goose extends Obstacle {
 
 	public String direction = "South"; //Facing south by default.
+	public Lane givenLane;
 	
-	public Goose(int xPosition, int yPosition, Texture texture) {
+	public Goose(int xPosition, int yPosition, Texture texture, Lane lane) {
 		// Joe: geese will have a set damage value, could increase with difficulty.
 		//      they'll also have a set width and height. just put some placeholders in for now.
 		// Ben: geese can also face N, S, E, W.
 		//      width and height will swap places when  switching between N/S and E/W.
 		super(5, xPosition, yPosition, texture.getWidth(), texture.getHeight(), texture);
+		this.givenLane = lane;
 	}
 	
 	public void changeDirection() {
@@ -33,20 +35,48 @@ public class Goose extends Obstacle {
 	}
 
 	public void Move(float moveVal) {
+		if(this.getX() > givenLane.GetLeftBoundary() || this.getX() < givenLane.GetRightBoundary()) {
+			if(direction == "South" || direction == "West") {
+				moveVal = moveVal * -1;
+			}
+			if(this.direction == "East" || this.direction == "West") {
+				this.setX(this.getX() + moveVal);
+				this.changeDirection();
+			}
+			if(direction == "North" || direction == "South") {
+				this.setY(this.getY() + moveVal);
+				int randomMove = 100;
+				if(new Random().nextInt(randomMove) == randomMove - 1) {
+					changeDirection();
+				}
+			}
+		}
+		else {
+			this.direction = "South";
+			this.setY(this.getY() - moveVal);
+		}
+		/*
+		boolean edge = false;
+		if(this.getX() == givenLane.GetLeftBoundary() || this.getX() == givenLane.GetRightBoundary()) {
+			edge = true;
+		}
 		
-		int randomMove = 10;
-		if(new Random().nextInt(randomMove) == randomMove) {
+		int randomMove = 100;
+		if(new Random().nextInt(randomMove) == randomMove - 1) {
 			changeDirection();
 		}
 		if(direction == "South" || direction == "West") {
 			moveVal = moveVal * -1;
 		}
 		if(direction == "North" || direction == "South") {
-			this.setY(this.getY() + moveVal);
+			this.setY(this.getY() + moveVal/2);
 		}
 		else {
-			this.setX(this.getX() + moveVal);
+			if(!edge) {
+				this.setX(this.getX() + moveVal/2);
+			}
 		}
+		*/
 	}
 	
 }
