9744ce73f53230b23c5a5c2c34f192eeb630d8a7
Joe  Lonsdale <jl3067@york.ac.uk>
2020-11-15 01:07:07 +0000

    Made the window un-resizable.

--- desktop/src/com/dragonboat/game/desktop/DesktopLauncher.java
+++ desktop/src/com/dragonboat/game/desktop/DesktopLauncher.java
@@ -9,6 +9,7 @@ public class DesktopLauncher {
 		LwjglApplicationConfiguration config = new LwjglApplicationConfiguration();
 		config.width = 1080;
 		config.height = 720;
+		config.resizable = false;
 		config.backgroundFPS = 60;
 		config.foregroundFPS = 60;
 		new LwjglApplication(new DragonBoatGame(), config);
