6d32d808a7e97087ad6d8e6afda509b20cced436
Omar Omar <omarhatem5555@gmail.com>
2021-02-04 12:26:54 +0000

    removed old test file

--- core/src/test/java/test1.java
+++
@@ -1,9 +0,0 @@
-import org.junit.jupiter.api.*;
-
-public class test1 {
-    @Test
-    public void firstTest() {
-        Assertions.assertTrue(true);
-        System.out.println("Done!");
-    }
-}
